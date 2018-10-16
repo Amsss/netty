31c16ade3cf50798e4684cd36db97ee0341efff7
zhzz1993 <zhuzz1030@163.com>
2018-10-16 11:56:18 +0800

    数据传输

---
+++ src/main/java/com/zhuzz/transport/ChannelOperationExamples.java
@@ -0,0 +1,77 @@
+package com.zhuzz.transport;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.CharsetUtil;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * Listing 4.5 Writing to a Channel
+ *
+ * Listing 4.6 Using a Channel from many threads
+ *
+ * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
+ */
+public class ChannelOperationExamples {
+    private static final Channel CHANNEL_FROM_SOMEWHERE = new NioSocketChannel();
+    /**
+     * Listing 4.5 Writing to a Channel
+     */
+    public static void writingToChannel() {
+        Channel channel = CHANNEL_FROM_SOMEWHERE; // Get the channel reference from somewhere
+        // 创建持有要写的ByteBuf
+        ByteBuf buf = Unpooled.copiedBuffer("your data", CharsetUtil.UTF_8);
+        // 写数据并冲刷它
+        ChannelFuture cf = channel.writeAndFlush(buf);
+        // 添加ChannelFutureListener以便在写操作完成后接收通知
+        cf.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) {
+                if (future.isSuccess()) {
+                    // 写操作完成，并且没有错误发生
+                    System.out.println("Write successful");
+                } else {
+                    // 记录错误
+                    System.err.println("Write error");
+                    future.cause().printStackTrace();
+                }
+            }
+        });
+    }
+
+    public static void test() {
+
+    }
+
+    /**
+     * Listing 4.6 Using a Channel from many threads
+     * ？？？消息被保证按顺序发送
+     */
+    public static void writingToChannelFromManyThreads() {
+        final Channel channel = CHANNEL_FROM_SOMEWHERE; // Get the channel reference from somewhere
+        // 创建持有要写的ByteBuf
+        final ByteBuf buf = Unpooled.copiedBuffer("your data",
+                CharsetUtil.UTF_8);
+        // 创建将数据写到Channel的Runable
+        Runnable writer = new Runnable() {
+            @Override
+            public void run() {
+                channel.write(buf.duplicate());
+            }
+        };
+        Executor executor = Executors.newCachedThreadPool();
+
+        // write in one thread
+        executor.execute(writer);
+
+        // write in another thread
+        executor.execute(writer);
+        //...
+    }
+}

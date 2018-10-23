ecf87752bafaa1c2bb51d3a3e1c8e50dcf1ae56d
Amsss <906602554@qq.com>
2018-10-23 16:22:50 +0800

    ChannelHandler单元测试 embedded

---
+++ src/main/java/com/zhuzz/embedded/AbsIntegerEncoderTest.java
@@ -0,0 +1,34 @@
+package com.zhuzz.embedded;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.embedded.EmbeddedChannel;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Listing 9.4 Testing the AbsIntegerEncoder
+ *
+ * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
+ */
+public class AbsIntegerEncoderTest {
+    @Test
+    public void testEncoded() {
+        ByteBuf buf = Unpooled.buffer();
+        for (int i = 1; i < 10; i++) {
+            buf.writeInt(i * -1);
+        }
+
+        EmbeddedChannel channel = new EmbeddedChannel(
+            new AbsIntegerEncoder());
+        assertTrue(channel.writeOutbound(buf));
+        assertTrue(channel.finish());
+
+        // read bytes
+        for (int i = 1; i < 10; i++) {
+            assertEquals(java.util.Optional.ofNullable(i), channel.readOutbound());
+        }
+        assertNull(channel.readOutbound());
+    }
+}
